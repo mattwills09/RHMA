db08ebbe0d9a966cce1e2013e203424aec38ccf0
mattwills09 <mattwills09@gmail.com>
2019-06-07 14:16:42 -0400

    update server.js

--- server.js
+++ server.js
@@ -76,13 +76,6 @@ mongoose.connect(
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
 
-app.get("*", (req, res) => {
-  let url = path.join(__dirname, '../client/build', 'index.html');
-  if (!url.startsWith('/app/')) // since we're on local windows
-    url = url.substring(1);
-  res.sendFile(url);
-});
-
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
